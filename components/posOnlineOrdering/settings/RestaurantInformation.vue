5656e45bb54d01638be4357920579885bf8803f3
chuoikhungbo <quanvantruong73@gmail.com>
2020-05-15 11:25:36 +0700

    add upload favicon

--- components/posOnlineOrdering/settings/RestaurantInformation.vue
+++ components/posOnlineOrdering/settings/RestaurantInformation.vue
@@ -45,15 +45,42 @@
         </div>
       </div>
       <div class="restaurant-info__main--right">
-        <div class="mb-3 fw-700">Upload photo</div>
-        <div class="mb-5">
-          <div class="mb-2">Store Photo</div>
-          <upload-zone :url="store.orderHeaderImageSrc" @url="update({ orderHeaderImageSrc: $event })" :option="{maxWidth: 500, maxHeight: 500}" :aspect-ratio="2"/>
-        </div>
         <div>
-          <div class="mb-2">Store Logo</div>
-          <upload-zone :url="store.logoImageSrc" @url="update({ logoImageSrc: $event })" :option="{maxHeight: 500, maxWidth: 500}"/>
+          <div class="mb-3 fw-700">Upload photo</div>
+          <div class="mb-5">
+            <div class="mb-2">Store Photo</div>
+            <upload-zone :url="store.orderHeaderImageSrc" @url="update({ orderHeaderImageSrc: $event })" :option="{maxWidth: 500, maxHeight: 500}" :aspect-ratio="2"/>
+          </div>
+          <div>
+            <div class="mb-2">Store Logo</div>
+            <upload-zone :url="store.logoImageSrc" @url="update({ logoImageSrc: $event })" :option="{maxHeight: 500, maxWidth: 500}"/>
+          </div>
         </div>
+        <template>
+          <div v-if="!store.faviconImageSrc">
+            <div class="mb-2 fw-700">Upload Favicon</div>
+            <div class="row-flex align-items-center">
+              <div style="font-size: 15px; font-weight: 300; font-style: italic; color: #757575">
+                Upload an icon for your online ordering website
+              </div>
+              <g-btn-bs style="margin-left: auto; margin-right: auto" @click="choosePhoto" icon="icon-upload_white" background-color="#536DFE" text-color="white">Upload</g-btn-bs>
+            </div>
+          </div>
+          <div v-else class="row-flex align-items-center">
+            <div class="mr-2">
+              <div class="mb-2 fw-700">Upload Favicon</div>
+              <div style="font-size: 15px; font-weight: 300; font-style: italic; color: #757575">
+                Choose a favicon image to be displayed
+              </div>
+            </div>
+            <div class="favicon">
+              <img alt :src="cdnUrl(store.faviconImageSrc)" style="width: 60px; height: 60px"/>
+              <div class="favicon--btn" @click="choosePhoto">
+                <g-icon color="#000" size="20">photo_camera</g-icon>
+              </div>
+            </div>
+          </div>
+        </template>
       </div>
     </div>
   </div>
@@ -61,11 +88,12 @@
 <script>
   import UploadZone from './UploadZone';
   import _ from 'lodash'
+  import {getCdnUrl} from "../../Store/utils";
 
   // TODO:
   // - remove old image when user change to new image
   // - input debounce
-  
+
   export default {
     name: 'RestaurantInformation',
     components: { UploadZone },
@@ -87,6 +115,7 @@
         country: this.store.country || ''
       }
     },
+    injectService: ['FileUploadStore:(openUploadFileDialog, uploadImage, showFileUploadProgressDialog, uploadingItems)'],
     computed: {
       iframe() {
         const storeUrl = [location.origin, 'store', this.store.alias].join('/');
@@ -102,6 +131,19 @@
       },
       async copyCode() {
         await navigator.clipboard.writeText(this.iframe)
+      },
+      async choosePhoto() {
+        this.openUploadFileDialog(async (file) => {
+          const src = await this.uploadImage(file)
+          this.$emit('update', {faviconImageSrc: src})
+        })
+        setTimeout(() => {
+          this.showFileUploadProgressDialog = false
+          this.uploadingItems = []
+        }, 3000)
+      },
+      cdnUrl(url) {
+        return `${getCdnUrl(url)}?w=60&h=60`
       }
     }
   }
@@ -118,20 +160,30 @@
     }
 
     &__main {
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-      grid-template-rows: 1fr;
-      grid-gap: 20px;
+      display: flex;
 
       &--left,
       &--right {
-        background-color: #FFF;
-        border-radius: 5px;
-        padding: 25px 25px 50px 25px;
+        flex: 0 0 calc(50% - 12px);
 
         .span-2 {
           grid-column: span 2;
         }
+
+        & > div {
+          background-color: #FFF;
+          border-radius: 5px;
+          padding: 24px;
+          margin-bottom: 24px;
+        }
+      }
+
+      &--left {
+        margin-right: 12px;
+      }
+
+      &--right {
+        margin-left: 12px;
       }
 
       .g-textarea {
@@ -176,6 +228,34 @@
 
       .bs-tf-wrapper ::v-deep .bs-tf-label {
         margin-bottom: 4px;
+      }
+
+      .favicon {
+        position: relative;
+        width: 80px;
+        height: 80px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        box-shadow: 2px 0 5px rgba(0, 0, 0, 0.15);
+        border-radius: 4px;
+        margin-left: auto;
+        margin-right: auto;
+
+        &--btn {
+          position: absolute;
+          bottom: -16px;
+          right: -16px;
+          width: 32px;
+          height: 32px;
+          border-radius: 50%;
+          background: #EFEFEF;
+          box-shadow: 1px 0 3px rgba(0, 0, 0, 0.40);
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          cursor: pointer;
+        }
       }
     }
   }
