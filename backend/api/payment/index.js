5847a381594c752a99920acb95d64f2e4b0eea48
ThinhVu <vutrongthinhk7@gmail.com>
2020-05-29 10:38:02 +0700

    Remove klanar service

--- backend/api/payment/index.js
+++ backend/api/payment/index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const paypal = require('./paypal')
-const klanar = require('./klanar')
 
 router.use('/paypal', paypal)
-router.use('/klanar', klanar)
 
 module.exports = router
