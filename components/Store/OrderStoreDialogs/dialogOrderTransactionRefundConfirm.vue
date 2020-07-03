c58812c903dbf786238745f4b6ed74d9b6fbf683
ThinhVu <vutrongthinhk7@gmail.com>
2020-07-03 11:40:55 +0700

    Update confirm dialog

--- components/Store/OrderStoreDialogs/dialogOrderTransactionRefundConfirm.vue
+++ components/Store/OrderStoreDialogs/dialogOrderTransactionRefundConfirm.vue
@@ -1,17 +1,32 @@
 <template>
   <dialog-common v-model="value" title="Refund order's transaction confirm">
     <div>Are you sure you want to refund this transaction?</div>
-    <div class="mt-2 pa-2" style="border-top: 1px dashed #aaa; border-bottom: 1px dashed #aaa;">
-      <div class="fw-700">ID: #{{order.id}}</div>
-      <div>Customer Name: {{order.customer.name}}</div>
-      <div>Customer Phone: {{order.customer.phone}}</div>
-      <div v-if="order.customer.address">
-        <div>Customer Address: {{order.customer.address}}</div>
-        <div>Zipcode: {{order.customer.zipCode}}</div>
+    <div class="mt-2" style="background-color: #eee; padding: 5px; border-radius: 5px;">
+      <div class="section-header">Customer info</div>
+      <div style="border-bottom: 1px dashed #aaa; padding-bottom: 10px;">
+        <div>Name: {{order.customer.name}}</div>
+        <div>Phone: {{order.customer.phone}}</div>
+        <div v-if="order.customer.address">
+          <div>Address: {{order.customer.address}}</div>
+          <div>Zipcode: {{order.customer.zipCode}}</div>
+        </div>
       </div>
-      <div>Price: {{$t('common.currency', storeLocale)}}{{order.payment[0].value}}</div>
-      <div>Order Time: {{order.date | formatDate}}</div>
-      <div>Delivery Time: {{order.deliveryTime}}</div>
+      
+      <div class="section-header mt-2">Order info</div>
+      <div>
+        <div>ID: #{{order.id}}</div>
+        <div>Total Amount: {{$t('common.currency', storeLocale)}}{{order.payment[0].value}}</div>
+        <div>Order Time: {{order.date | formatDate}}</div>
+        <div>Delivery Time: {{order.deliveryTime}}</div>
+        <div>
+          <div>Item list</div>
+          <ol style="margin: 0">
+            <li v-for="(item, i) in order.items">
+              {{ item.name }} x {{ item.quantity }}
+            </li>
+          </ol>
+        </div>
+      </div>
     </div>
     <template #actions>
       <g-btn-bs @click="close" width="100" style="margin: 0">Cancel</g-btn-bs>
@@ -45,3 +60,9 @@
     }
   }
 </script>
+<style lang="scss" scoped>
+  .section-header {
+    font-weight: bold;
+    font-size: 18px;
+  }
+</style>
