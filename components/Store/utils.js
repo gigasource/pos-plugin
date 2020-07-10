fc89b50af6e549533c5d068149167424a444a740
ThinhVu <vutrongthinhk7@gmail.com>
2020-07-10 10:29:51 +0700

    Allow custom cdn domain name

--- components/Store/utils.js
+++ components/Store/utils.js
@@ -1,6 +1,6 @@
 export function getCdnUrl(url) {
-  if (cms.sharedConfig && cms.sharedConfig.getCdnUrl) return cms.sharedConfig.getCdnUrl(url)
-
+  if (cms.sharedConfig && cms.sharedConfig.getCdnUrl && cms.sharedConfig.CDN_DOMAIN_NAME)
+    return cms.sharedConfig.getCdnUrl(url, cms.sharedConfig.CDN_DOMAIN_NAME)
   return url
 }
 
