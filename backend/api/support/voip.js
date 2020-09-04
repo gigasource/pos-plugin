cbc6be2fc57d575bc32a7554c321c3293b52aa18
ThinhVu <vutrongthinhk7@gmail.com>
2020-09-04 15:35:49 +0700

    ADd call manager to manage device callable status

--- backend/api/support/voip.js
+++ backend/api/support/voip.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const {firebaseAdminInstance} = require('../../app-notification/firebase-messaging/admin')
+const _ = require('lodash')
+const callMgr = require('./callMgr')
+const CallStatus = callMgr.CallStatus;
 
 const fcm = {
   getFirebaseToken: async function(deviceId) {
@@ -57,17 +60,24 @@ const fcm = {
 
 // make call
 router.post('/make-call', async (req, res) => {
-
   const { sender, receiver } = req.body
   console.log(`make-call from ${sender} to ${receiver}`)
-  fcm.send(receiver, 'makeCall', {sender})
-  res.json({ok:true})
+  if (callMgr.getStatus(receiver) === CallStatus.Idle) {
+    console.log('Receiver is idle, can make a call')
+    callMgr.makeCall(sender, receiver)
+    fcm.send(receiver, 'makeCall', {sender})
+    res.json({ok:true})
+  } else {
+    console.log("Receiver is busy, can't make a call")
+    res.json({ok:false, error: 'Receiver busy'})
+  }
 })
 
 // answer/declined
 router.post('/make-call-ack', async (req, res) => {
   const { sender, receiver, callAccepted } = req.body
   console.log(`make-call-ack from ${sender} to ${receiver}`)
+  callMgr.makeCallAck(sender, receiver, callAccepted)
   fcm.send(receiver, 'makeCallAck', {sender, callAccepted})
   res.json({ok:true})
 })
@@ -75,6 +85,7 @@ router.post('/make-call-ack', async (req, res) => {
 router.post('/cancel-call', async (req, res) => {
   const { sender, receiver } = req.body
   console.log(`cancel-call from ${sender} to ${receiver}`)
+  callMgr.cancelCall(sender, receiver)
   fcm.send(receiver, 'cancelCall', {sender})
   res.json({ok:true})
 })
@@ -83,17 +94,35 @@ router.post('/cancel-call', async (req, res) => {
 router.post('/end-call', async (req, res) => {
   const { sender, receiver } = req.body
   console.log(`end-call from ${sender} to ${receiver}`)
+  callMgr.endCall(sender, receiver)
   fcm.send(receiver, 'endCall', {sender})
   res.json({ok:true})
 })
 
 // get device id of supporters
-router.get('/get-supporters', async (req, res) => {
+router.get('/supporters/:storeId', async (req, res) => {
+  const { storeId } = req.params
+  const store = await cms.getModel('Store').findOne({ _id: storeId })
+
+  if (!store) {
+    res.json({ ok: false, error: "Store id is invalid" })
+    return
+  }
+
+  // select first user which is not an admin, and also in idle state
+  const sgIds = store.groups.map(sg => sg._id)
+  const users = await cms.getModel('User').find({ username: { $ne: 'admin' },  storeGroups: { $elemMatch: { $in: sgIds } } }, { _id: 1, username: 1 })
+  const selectedUser = _.find(users, u => callMgr.getStatus(u._id) !== CallStatus.Busy)
+
   res.json({
-    ok: true,
-    supporter: '5e96bdee046a735999b167eb'
+    ok: selectedUser !== null,
+    supporter: selectedUser._id
   })
 })
 
+router.get('/device-status/:deviceId', async (req, res) => {
+  const {deviceId} = req.params
+  res.json({ status: callMgr.getStatus(deviceId) })
+})
 
 module.exports = router
