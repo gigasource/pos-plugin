2b5cb71115829c4e44d88d83b5a1d841a1a2592a
chuoikhungbo <quanvantruong73@gmail.com>
2020-09-08 10:52:47 +0700

    add slide show logo for franchise page

--- components/posOnlineOrdering/franchise/FranchiseView.vue
+++ components/posOnlineOrdering/franchise/FranchiseView.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="background">
+    <g-icon size="20" @click="close" class="icon-close">icon-close</g-icon>
     <section class="franchise-view r">
-      <div v-if="mainStore && mainStore.logoImageSrc" class="logo">
-        <img alt :src="cdnStoreLogoImage"/>
-      </div>
-      <div @click="close" class="abs" style="top: 20px; right: 20px; width: 20px; height: 20px; cursor: pointer">
-        <img alt src="/plugins/pos-plugin/assets/close.svg" draggable="false"/>
+      <div v-if="stores && logos && logos.length > 0" class="logo">
+        <g-slideshow style="background-color: transparent" v-model="logos" />
       </div>
       <div class="tilte">Please select a restaurant</div>
       <div class="store-cards">
@@ -18,15 +16,16 @@
 <script>
   import StoreCard from './StoreCard';
   import { getCdnUrl } from '../../Store/utils';
+  import GSlideshow from "../../../../../backoffice/pos-vue-framework/src/components/GSlideshow/GSlideshow";
 
   export default {
     name: 'FranchiseView',
-    components: { StoreCard },
+    components: { StoreCard, GSlideshow },
     props: {},
     data: function () {
       return {
         stores: [],
-        mainStore: null
+        logos: []
       }
     },
     async created() {
@@ -38,11 +37,15 @@
 
       const stores = await cms.getModel('Store').find({ clientDomain })
       this.stores.splice(0, 0, ...stores)
+      this.logos = stores.map(store => ({
+        src: getCdnUrl(store.logoImageSrc),
+        type: 'image',
+        transition: 'slideRightToLeft',
+        duration: 5000
+      })).filter(logo => !!logo.src)
     },
     computed: {
-      cdnStoreLogoImage() {
-        return this.mainStore.logoImageSrc && getCdnUrl(this.mainStore.logoImageSrc || '/plugins/pos-plugin/assets/images/logo.png')
-      }
+
     },
     methods: {
       close() {
@@ -56,21 +59,34 @@
     background: white url("../../../assets/images/franchise-bg.png");
     background-size: cover;
     min-height: 100vh;
+    width: 60%;
+    max-width: 1140px;
+    align-self: center;
+    position: relative;
+  }
+
+  .icon-close {
+    position: absolute;
+    top: 20px;
+    right: 20px;
   }
 
   .logo {
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    position: relative;
+    width: 100%;
+    height: 150px;
 
-    & > img {
-      max-width: 150px;
+    ::v-deep img {
+      object-fit: unset;
+      max-width: 100%;
+      width: auto;
+      margin-left: 50%;
+      transform: translateX(-50%);
     }
   }
 
   .franchise-view {
     width: 100%;
-    max-width: 1032px;
     margin: 36px auto;
     padding: 0 36px;
   }
@@ -83,6 +99,10 @@
   }
 
   @media screen and (max-width: 640px) {
+    .background {
+      width: 100%;
+    }
+
     .franchise-view {
       padding: 36px 8px;
       margin-top: 0;
