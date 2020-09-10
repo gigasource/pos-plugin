1f6bc727623d582da0951607afc11d085bba0d77
danglam105 <danglam105@gmail.com>
2020-09-10 10:34:10 +0700

    Allow passing arguments in pos commands

--- backend/online-order/pos-command-receiver.js
+++ backend/online-order/pos-command-receiver.js
@@ -9,11 +9,19 @@ const commandMap = {
 
 function init(socket) {
   socket.on('posCommand:send', async (command, ackFn) => {
+    const commandArray = command.split(' ');
+    let commandArgs = [];
+
+    if (commandArray.length > 1) {
+      command = commandArray.shift();
+      commandArgs = commandArray;
+    }
+
     if (!commandMap[command]) {
       ackFn('Error: Invalid command');
     } else {
       const fn = commandMap[command];
-      const commandResult = await Promise.resolve(fn());
+      const commandResult = await Promise.resolve(fn(...commandArgs));
       ackFn(commandResult);
     }
   });
