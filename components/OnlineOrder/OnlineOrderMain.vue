2e11597e5b8eb6b7f7d4abc7b06bbdc39bb272fe
chuoikhungbo <quanvantruong73@gmail.com>
2020-09-28 17:18:03 +0700

    fix show both calls and missed calls

--- components/OnlineOrder/OnlineOrderMain.vue
+++ components/OnlineOrder/OnlineOrderMain.vue
@@ -16,7 +16,7 @@
             <p>{{$t('onlineOrder.noPending')}}</p>
           </div>
         </template>
-        <template v-else-if="(!internalOrders || !internalOrders.length) && calls.length > 0">
+        <template v-else-if="(!internalOrders || !internalOrders.length) && (calls.length > 0 || missedCalls.length > 0)">
           <div class="pending-orders--call" v-for="(call, i) in calls" :key="`call_${i}`">
             <div class="pending-orders--call-title">
               <div>{{call.customer.name}} <span>-</span> {{call.customer.phone}}</div>
@@ -39,10 +39,8 @@
               </g-btn-bs>
             </div>
           </div>
-        </template>
-        <template v-else-if="(!internalOrders || !internalOrders.length) && missedCalls.length > 0">
           <div class="pending-orders--call b-red" v-for="(call, i) in missedCalls" :key="`call_${i}`"
-              v-touch="getTouchHandlers(i)">
+               v-touch="getTouchHandlers(i)">
             <div class="pending-orders--call-title">
               <div>{{call.customer.name}} <span>-</span> {{call.customer.phone}}</div>
               <g-spacer/>
