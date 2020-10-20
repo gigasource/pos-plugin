3ddef201ef20ac679faccc8292548aceab7eaf21
ThinhVu <vutrongthinhk7@gmail.com>
2020-10-20 11:34:11 +0700

    exclude inventory commit

--- backend/master-node/collection-commit/index.js
+++ backend/master-node/collection-commit/index.js
@@ -2,6 +2,6 @@ module.exports = async function (updateCommit) {
 	await require('./orderCommit')(updateCommit);
 	await require('./reportCommit')(updateCommit);
 	await require('./posCommit')(updateCommit);
-	await require('./inventory/inventoryCategoryCommit')(updateCommit);
-	await require('./inventory/inventoryCommit')(updateCommit);
+	// await require('./inventory/inventoryCategoryCommit')(updateCommit);
+	// await require('./inventory/inventoryCommit')(updateCommit);
 }
--- backend/master-node/master.js
+++ backend/master-node/master.js
@@ -121,8 +121,8 @@ class Master {
 		updateCommit.getMethod('order', 'resumeQueue')();
 		updateCommit.getMethod('report', 'resumeQueue')();
 		updateCommit.getMethod('pos', 'resumeQueue')();
-		updateCommit.getMethod('inventory', 'resumeQueue')();
-		updateCommit.getMethod('inventoryCategory', 'resumeQueue')();
+		// updateCommit.getMethod('inventory', 'resumeQueue')();
+		// updateCommit.getMethod('inventoryCategory', 'resumeQueue')();
 	}
 
 	emitToAll(commits) {
