e3bd60ea031978522c9df8c1071da082ad132239
chuoikhungbo <quanvantruong73@gmail.com>
2020-10-22 18:59:52 +0700

    fix bug negative discount with last item price = 0

--- components/logic/orderUtil.js
+++ components/logic/orderUtil.js
@@ -24,11 +24,11 @@ const orderUtil = {
     const totalWithoutDiscountResist = difference + value;
     const percent =  difference / totalWithoutDiscountResist * 100;
     let sumDiscount = 0;
-    const lastDiscountableItemIndex = _.findLastIndex(items, item => !item.discountResistance);
+    const lastDiscountableItemIndex = _.findLastIndex(items, item => !item.discountResistance && item.originalPrice > 0);
     for (let i = 0; i < items.length; i++) {
       let item = items[i];
       if (!item.discountResistance) {
-        if (i < lastDiscountableItemIndex) {
+        if (i !== lastDiscountableItemIndex) {
           item.price = +(item.originalPrice * (100 - percent) / 100);
           item.discountUnit = 'percent';
           item.vDiscount = +(item.originalPrice - item.price)
