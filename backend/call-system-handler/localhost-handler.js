51f4fae60c2b2ad48602b58c3d867d652e04d9f7
danglam105 <danglam105@gmail.com>
2020-11-03 17:14:05 +0700

    Update CallSystem view

---
+++ backend/call-system-handler/localhost-handler.js
@@ -0,0 +1,97 @@
+module.exports = async (cms) => {
+  const {CallMonitor} = require('fritz-callmonitor');
+  let callMap = {};
+  let fritzboxMonitor;
+  let connectionStatus = '';
+
+  cms.socket.on('connect', internalSocket => {
+    internalSocket.on('refresh-call-system-config', setupLocalFritzbox);
+    internalSocket.on('get-call-system-status', updateConnectionStatus);
+  });
+
+  async function updateConnectionStatus(cb, posSettings) {
+    if (!posSettings) posSettings = await cms.getModel('PosSetting').findOne();
+    const {call: callConfig} = posSettings;
+    let {mode} = callConfig;
+    const useLocalFritzbox = mode === 'localhost-fritzbox';
+
+    if (cb) cb(useLocalFritzbox ? connectionStatus : null);
+    else cms.socket.emit('update-call-system-status', useLocalFritzbox ? connectionStatus : null);
+  }
+
+  await setupLocalFritzbox();
+
+  function reset() {
+    if (fritzboxMonitor) {
+      fritzboxMonitor.end();
+      fritzboxMonitor.socket.destroy();
+      fritzboxMonitor.removeAllListeners();
+      fritzboxMonitor = null;
+    }
+    callMap = {};
+  }
+
+  async function setupLocalFritzbox() {
+    const posSettings = await cms.getModel('PosSetting').findOne();
+    const {call: callConfig} = posSettings;
+    let {mode, ipAddresses = {}} = callConfig;
+    const useLocalFritzbox = mode === 'localhost-fritzbox';
+
+    if (!useLocalFritzbox && fritzboxMonitor) {
+      reset();
+    } else if (useLocalFritzbox) {
+      reset();
+      let fritzboxIp = ipAddresses[mode] || '192.168.12.1';
+      let fritzboxPort = 1012;
+
+      if (fritzboxIp.includes(':')) {
+        const arr = fritzboxIp.split(':');
+        fritzboxIp = arr[0];
+        fritzboxPort = +arr[1];
+      }
+
+      fritzboxMonitor = new CallMonitor(fritzboxIp, fritzboxPort);
+      connectionStatus = 'Connecting...';
+      await updateConnectionStatus(null, posSettings);
+
+      fritzboxMonitor.on("ring", async (callInfo) => {
+        const {connectionId, caller} = callInfo;
+        callMap[connectionId] = {pickedUp: false, caller}
+
+        cms.socket.emit('new-phone-call', callInfo.caller, new Date());
+      });
+
+      fritzboxMonitor.on("pickup", (callInfo) => {
+        const {connectionId} = callInfo;
+        if (callMap[connectionId]) callMap[connectionId].pickedUp = true;
+      });
+
+      fritzboxMonitor.on("hangup", async (callInfo) => {
+        const {connectionId} = callInfo;
+        const callSession = callMap[connectionId];
+
+        if (callSession && !callSession.pickedUp) {
+          cms.socket.emit('new-missed-phone-call', callSession.caller, new Date());
+          delete callMap[connectionId];
+        }
+      });
+
+      fritzboxMonitor.on('end', () => {
+        connectionStatus = 'Disconnected';
+        updateConnectionStatus();
+        console.log('Disconnected from Fritzbox device');
+      });
+      fritzboxMonitor.on('connect', () => {
+        connectionStatus = 'Connected';
+        updateConnectionStatus();
+        console.log('Connected to Fritzbox device');
+      });
+      fritzboxMonitor.on('error', err => {
+        connectionStatus = 'Connection error, please check your Fritzbox IP address';
+        updateConnectionStatus();
+        console.error(err);
+      });
+      fritzboxMonitor.connect();
+    }
+  }
+}
--- components/OnlineOrder/CallSystem.vue
+++ components/OnlineOrder/CallSystem.vue
@@ -1,123 +1,196 @@
 <template>
   <div class="call">
-    <p class="call-title mb-2">Call System</p>
-    <g-switch label="Active" v-model="active"/>
-    <g-switch :label="demoModeLabel" v-model="demoMode"/>
-    <p class="call-title">Call System IP</p>
-    <g-text-field-bs v-model="ip">
-      <template v-slot:append-inner>
-        <g-icon @click="dialog.ip = true">icon-keyboard</g-icon>
-      </template>
-    </g-text-field-bs>
+    <p class="call-title mb-2">{{ `Call System${callSystemStatus}` }}</p>
+    <div class="row-flex align-items-center justify-start">
+      <g-grid-select class="mt-2"
+                     :items="callSystemModes"
+                     mandatory
+                     :grid="false"
+                     v-model="currentCallSystemMode">
+        <template #default="{ toggleSelect, item }">
+          <g-btn class="mx-1 mb-1" @click="toggleSelect(item)" :disabled="item === 'auto'">{{ item.text }}</g-btn>
+        </template>
+        <template #selected="{ toggleSelect, item }">
+          <g-btn class="mx-1 mb-1" @click="toggleSelect(item)" background-color="blue" text-color="white">
+            {{ item.text }}
+          </g-btn>
+        </template>
+      </g-grid-select>
+    </div>
+
+    <div v-if="currentCallSystemMode !== CALL_SYSTEM_MODES.OFF.value">
+      <p class="call-title">{{ callSystemIpText }}</p>
+      <g-text-field-bs v-model="ipAddresses[currentCallSystemMode]">
+        <template v-slot:append-inner>
+          <g-icon @click="dialog.ip = true">icon-keyboard</g-icon>
+        </template>
+      </g-text-field-bs>
+    </div>
+
     <p class="call-title">Google Map API Key</p>
     <g-text-field-bs v-model="apiKey">
       <template v-slot:append-inner>
         <g-icon @click="dialog.api = true">icon-keyboard</g-icon>
       </template>
     </g-text-field-bs>
-    <g-btn-bs width="80" background-color="#2979FF" style="margin: 16px 0 0; flex: 0 0 36px; align-self: flex-end" @click="update">Update</g-btn-bs>
-    <dialog-text-filter v-model="dialog.ip" label="Call System IP" :default-value="ip" @submit="changeIp"/>
+    <g-btn-bs width="80" background-color="#2979FF" style="margin: 16px 0 0; flex: 0 0 36px; align-self: flex-end"
+              @click="update">Update
+    </g-btn-bs>
+    <dialog-text-filter v-model="dialog.ip" label="Call System IP" :default-value="''" @submit="changeIp"/>
     <dialog-text-filter v-model="dialog.api" label="Google Map API Key" :default-value="apiKey" @submit="changeApiKey"/>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "CallSystem",
-    data() {
-      return {
-        active: false,
-        demoMode: false,
-        demoModeStatus: '',
-        ip: '',
-        apiKey: '',
-        dialog: {
-          ip: false,
-          api: false,
-        },
-        lastSavedConfig: null,
+import cloneDeep from 'lodash/cloneDeep'
+
+const CALL_SYSTEM_MODES = {
+  OFF: {text: 'Off', value: 'off'},
+  FRITZBOX: {text: 'Localhost (Fritzbox)', value: 'localhost-fritzbox'},
+  DEMO: {text: 'Demo (Fritzbox)', value: 'demo-fritzbox'},
+}
+
+export default {
+  name: "CallSystem",
+  data() {
+    return {
+      callSystemModes: [CALL_SYSTEM_MODES.OFF, CALL_SYSTEM_MODES.FRITZBOX, CALL_SYSTEM_MODES.DEMO],
+      currentCallSystemMode: CALL_SYSTEM_MODES.OFF.value,
+      active: false,
+      callSystemConnectionStatus: '',
+      ipAddresses: {},
+      apiKey: '',
+      dialog: {
+        ip: false,
+        api: false,
+      },
+      lastSavedConfig: null,
+      CALL_SYSTEM_MODES,
+    }
+  },
+  async created() {
+    await this.loadData()
+    cms.socket.on('update-call-system-status', this.updateConnectStatus);
+    cms.socket.emit('get-call-system-status', this.updateConnectStatus);
+  },
+  async activated() {
+
+  },
+  computed: {
+    // demoModeLabel() {
+    //   if (!this.demoMode) {
+    //     return 'Demo mode'
+    //   } else {
+    //     if (this.lastSavedConfig && !this.lastSavedConfig.demoMode && this.demoMode) {
+    //       return "Demo mode (press 'Update' to connect)"
+    //     } else {
+    //       return `Demo mode${this.demoModeStatus ? ` (${this.demoModeStatus})` : ''}`
+    //     }
+    //   }
+    // },
+    callSystemConfigChanged() {
+      return !(this.lastSavedConfig
+          && this.lastSavedConfig.mode === this.currentCallSystemMode
+          && this.lastSavedConfig.ipAddresses[this.currentCallSystemMode] === this.ipAddresses[this.currentCallSystemMode])
+    },
+    callSystemStatus() {
+      if (!this.callSystemConfigChanged && this.currentCallSystemMode === CALL_SYSTEM_MODES.OFF.value) return '';
+
+      if (this.callSystemConfigChanged) {
+        return " (Call system config has changed, press 'Update' to apply changes)";
+      } else {
+        return ` (${this.callSystemConnectionStatus})`;
       }
     },
-    async created() {
+    callSystemIpText() {
+      switch (this.currentCallSystemMode) {
+        case CALL_SYSTEM_MODES.FRITZBOX.value:
+          return 'Fritzbox local IP';
+        case CALL_SYSTEM_MODES.DEMO.value:
+          return 'Fritzbox proxy server address';
+        default:
+          return '';
+      }
+    }
+  },
+  watch: {
+    currentCallSystemMode(value) {
+      let defaultValue = '';
+
+      if (value === CALL_SYSTEM_MODES.DEMO.value) defaultValue = 'https://fritzbox-proxy-10000.gigasource.io';
+      else if (value === CALL_SYSTEM_MODES.FRITZBOX.value) defaultValue = '192.168.178.1:1012';
+      else if (value === CALL_SYSTEM_MODES.OFF.value) defaultValue = '';
+
+      this.ipAddresses[value] = this.ipAddresses[value] || defaultValue;
+    }
+  },
+  methods: {
+    async loadData() {
+      const callSystem = (await cms.getModel('PosSetting').findOne())['call']
+      callSystem.mode = callSystem.mode || CALL_SYSTEM_MODES.OFF.value
+      callSystem.ipAddresses = callSystem.ipAddresses || {};
+
+      this.active = callSystem.active
+      this.ipAddresses = callSystem.ipAddresses || {}
+      this.apiKey = callSystem.googleMapApiKey
+      this.currentCallSystemMode = callSystem.mode
+      this.lastSavedConfig = cloneDeep(callSystem)
+    },
+    async update() {
+      const configChanged = this.callSystemConfigChanged;
+      const call = {
+        active: this.active,
+        ipAddresses: this.ipAddresses,
+        googleMapApiKey: this.apiKey,
+        mode: this.currentCallSystemMode,
+      }
+      cms.socket.emit('get-call-system-status', this.updateConnectStatus);
+      await cms.getModel('PosSetting').findOneAndUpdate({}, {call})
       await this.loadData()
-      cms.socket.on('update-fritzbox-demo-status', (status) => this.demoModeStatus = status);
-      cms.socket.emit('get-fritzbox-demo-status', (status) => this.demoModeStatus = status);
+      if (configChanged) cms.socket.emit('refresh-call-system-config')
     },
-    async activated() {
-      await this.loadData()
+    changeIp(value) {
+      this.ipAddresses[this.currentCallSystemMode] = value
     },
-    computed: {
-      demoModeLabel() {
-        if (!this.demoMode) {
-          return 'Demo mode'
-        } else {
-          if (this.lastSavedConfig && !this.lastSavedConfig.demoMode && this.demoMode) {
-            return "Demo mode (press 'Update' to connect)"
-          } else {
-            return `Demo mode${this.demoModeStatus ? ` (${this.demoModeStatus})` : ''}`
-          }
-        }
-      },
+    changeApiKey(value) {
+      this.apiKey = value
     },
-    methods: {
-      async loadData() {
-        const callSystem = (await cms.getModel('PosSetting').findOne())['call']
-        this.active = callSystem.active
-        this.ip = callSystem.ip
-        this.apiKey = callSystem.googleMapApiKey
-        this.demoMode = callSystem.demoMode
-        this.lastSavedConfig = callSystem
-      },
-      async update() {
-        const call = {
-          active: this.active,
-          ip: this.ip,
-          googleMapApiKey: this.apiKey,
-          demoMode: this.demoMode,
-        }
-        await cms.getModel('PosSetting').findOneAndUpdate({}, { call })
-        await this.loadData()
-        cms.socket.emit('refresh-fritzbox-config')
-      },
-      changeIp(value) {
-        this.ip = value
-      },
-      changeApiKey(value) {
-        this.apiKey = value
-      },
+    updateConnectStatus(status) {
+      if (status) this.callSystemConnectionStatus = status;
     }
   }
+}
 </script>
 
 <style scoped lang="scss">
-  .call {
-    width: 65%;
-    display: flex;
-    flex-direction: column;
-    background-color: white;
-    padding-left: 24px;
-    height: 100%;
-    overflow: auto;
+.call {
+  width: 65%;
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  padding-left: 24px;
+  height: 100%;
+  overflow: auto;
 
-    &-title {
-      font-size: 14px;
-      font-weight: bold;
-      margin-top: 12px;
-      margin-bottom: 4px;
-    }
+  &-title {
+    font-size: 14px;
+    font-weight: bold;
+    margin-top: 12px;
+    margin-bottom: 4px;
+  }
 
-    .bs-tf-wrapper {
-      margin: 0;
-      width: 100%;
+  .bs-tf-wrapper {
+    margin: 0;
+    width: 100%;
 
-      ::v-deep .input {
-        flex: 1;
-        padding-right: 12px;
-      }
-    }
-
-    .g-switch-wrapper {
-      margin: 4px;
+    ::v-deep .input {
+      flex: 1;
+      padding-right: 12px;
     }
   }
+
+  .g-switch-wrapper {
+    margin: 4px;
+  }
+}
 </style>
