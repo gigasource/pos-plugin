4f86a88fc094efaff7c535f22166f63fd297b304
danglam105 <danglam105@gmail.com>
2020-12-01 14:07:10 +0700

    Remove unused logs & fix missing locale text content

--- i18n/en-AU.js
+++ i18n/en-AU.js
@@ -1,6 +1,10 @@
+let enLocale = require('./en').en;
+
 module.exports = {
   'en-AU': {
+    ...enLocale,
     common: {
+      ...enLocale.common,
       currency: '$',
       currencyCode: "AUD",
       countryCode: "AU",
--- i18n/en-GB.js
+++ i18n/en-GB.js
@@ -1,6 +1,10 @@
+let enLocale = require('./en').en;
+
 module.exports = {
   'en-GB': {
+    ...enLocale,
     common: {
+      ...enLocale.common,
       currency: '£',
       currencyCode: "GBP",
       countryCode: "GB",
