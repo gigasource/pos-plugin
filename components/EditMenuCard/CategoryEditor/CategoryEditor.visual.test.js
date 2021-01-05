a0285d84d1ebe7c5db6ec1471e8a61867c159bef
ThinhVu <vutrongthinhk7@gmail.com>
2021-01-05 18:39:34 +0700

    Add CategoryEditor draft component

--- components/EditMenuCard/CategoryEditor/CategoryEditor.visual.test.js
+++ components/EditMenuCard/CategoryEditor/CategoryEditor.visual.test.js
@@ -1,5 +1,11 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import { reactive } from 'vue'
+import { mount } from '@vue/test-utils'
 import CategoryEditorFactory from './CategoryEditorFactory'
+import cms from '../../../../../backoffice/src/cms';
 
 describe('Visual:CategoryEditor', function () {
   beforeEach(() => {
@@ -10,21 +16,32 @@ describe('Visual:CategoryEditor', function () {
 
   })
 
-  describe('Categories Layout', () => {
-    const { hooks, fn } = CategoryEditorFactory()
+  describe('Categories_Layout', () => {
+    it('should', async () => {
+      const { hooks, fn } = CategoryEditorFactory()
+      hooks.on('renderPopUp', function(
+          renderCategoryLayoutSetting,
+          renderCateNameSetting,
+          renderCateColorSetting,
+          renderCateProductLayoutSetting,
+          renderPopUp) {
+        this.update('renderCategoryEditor', () => (
+            <>
+              { renderCategoryLayoutSetting() }
+            </>
+        ))
+      })
 
-
-    hooks.on('renderPopUp', function(
-        renderCategoryLayoutSetting,
-        renderCateNameSetting,
-        renderCateColorSetting,
-        renderCateProductLayoutSetting,
-        renderPopUp) {
-      this.update('renderCategoryLayoutSetting', () => (
-        <>
-          { renderCategoryLayoutSetting() }
-        </>
-      ))
+      const component = fn()
+      const i18n = createI18n({
+        locale: cms.locale,
+        fallbackLocale: 'en',
+        messages: cms.i18n
+      })
+      const wrapper = mount(component, {
+        plugins: [i18n]
+      })
+      expect(wrapper.html()).toMatchSnapshot()
     })
   })
 
