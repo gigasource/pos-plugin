3e536337eb8303c8177d295b33e7d36552b3e003
longnh301 <43654367+longnh301@users.noreply.github.com>
2021-01-06 10:49:18 +0700

    Vue 3 new components (#186)
    * added new components
    
    * fix class properties bug
    
    * fix class properties bug
    
    * updated package.json
    
    * Add CategoryEditor draft component
    
    * added new components
    
    * fix class properties bug
    
    * fix class properties bug
    
    * updated package.json
    
    * Add CategoryEditor draft component
    
    * merge manifest
    
    Co-authored-by: Triracle <triracle@gmail.com>
    Co-authored-by: ThinhVu <vutrongthinhk7@gmail.com>

---
+++ test-utils/setupTest.js
@@ -0,0 +1,83 @@
+const { VueLoaderPlugin } = require('vue-loader')
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = async function (options) {
+	const config = {
+		entry: options.componentPath,
+		output: {
+			libraryTarget: 'commonjs2',
+			publicPath: '', // prevent prefix 'auto'
+			path: path.resolve(__dirname, '../../dist_test')
+		},
+		resolve: {
+			extensions: ['.vue', '.jsx', '.js', '.json'],
+			alias: {
+				vue: "@vue/runtime-dom",
+			},
+		},
+		externals: {
+			vue: 'vue'
+		},
+		optimization: {
+			minimize: false,
+		},
+		module: {
+			rules: [
+				{
+					test: /\.vue$/,
+					use: "vue-loader",
+				},
+				{
+					test: /\.png$/,
+					use: {
+						loader: "url-loader",
+						options: { limit: 8192 },
+					},
+				},
+				{
+					test: /\.js?$/,
+					loader: "babel-loader",
+					options: {
+						plugins: [
+							[
+								"@babel/plugin-proposal-class-properties"
+							]
+						]
+					}
+				},
+				{
+					test: /\.css$/,
+					use: [
+						{
+							loader: MiniCssExtractPlugin.loader,
+						},
+						"css-loader",
+					],
+				},
+			],
+		},
+		plugins: [
+			new MiniCssExtractPlugin({
+				filename: "[name].css",
+			}),
+			new VueLoaderPlugin(),
+			new WebpackManifestPlugin({ fileName: 'ssr-manifest.json' })
+		]
+	}
+
+	return new Promise((resolve, reject) => {
+		webpack(config, (err, stats) => {
+			if (err) {
+				return reject(err)
+			}
+			if (stats.hasErrors()) {
+				return reject(stats.compilation.errors)
+			}
+			console.log('Build complete.')
+			resolve()
+		})
+	})
+}
