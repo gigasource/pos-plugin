07c2491516e09f24521987356b4a43003968066d
ThinhVu <vutrongthinhk7@gmail.com>
2021-01-20 11:33:34 +0700

    Add StubFactory to fix bug renderDefaultSlot Fix migrate mistake Add test & snapshot for product

---
+++ components2/EditMenuCard/__test__/category-editor.dom.test.js
@@ -0,0 +1,52 @@
+import {
+  actions,
+  cola,
+  drinkTax,
+  expectArray,
+  fanta,
+  foodTax,
+  ketchup,
+  rice,
+  component,
+  wrapper,
+  makeWrapper,
+  orm,
+  setComponent
+} from "../../test-utils";
+const delay = require("delay");
+import CategoryEditor2 from '../CategoryEditor/CategoryEditor2';
+import {nextTick} from 'vue'
+import { updateOrderLayout, updateSelectedCategoryLayout } from '../../OrderView/pos-ui-shared';
+const ignoreComponents = ['Portal', 'PortalTarget', 'MountingPortal', 'portal', 'portal-target']
+
+describe('category-editor', () => {
+  it('should show correct', async () => {
+    updateOrderLayout({ rows: 2, columns: 4 })
+    updateSelectedCategoryLayout({ name: 'Wine', color: '#FFF', rows: 4, columns: 6,  })
+
+    // wipe-out components (to make dependencies component un-resolvable) to make sure snapshot render prop for stub class
+    CategoryEditor2.components = {}
+
+    makeWrapper(CategoryEditor2, {
+      shallow: false,
+      global: {
+        stubs: {
+          portal: true,
+          'dialog-confirm-delete': true,
+          'g-grid-select': true,
+          'color-selector': true,
+          'input-number': true,
+          'dialog-text-filter': true,
+        },
+        config: {
+          isCustomElement: tag => _.includes(ignoreComponents, tag)
+        }
+      }
+    }, false)
+    await nextTick()
+    await delay(50)
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  // act
+})
---
+++ components2/EditMenuCard/__test__/product-editor.dom.test.js
@@ -0,0 +1,56 @@
+import {config, mount} from "@vue/test-utils";
+import { wrapper, makeWrapper } from "../../test-utils";
+import { mockProduct } from "../../OrderView/__test__/mock_product";
+const delay = require("delay");
+import _ from 'lodash'
+import ProductEditor2 from '../ProductEditor/ProductEditor2';
+import {nextTick} from 'vue'
+import {
+  updateOrderLayout,
+  updateSelectedCategoryLayout,
+  updateSelectedProductLayout
+} from '../../OrderView/pos-ui-shared';
+import StubFactory from '../../StubFactory';
+const ignoreComponents = ['Portal', 'PortalTarget', 'MountingPortal', 'portal', 'portal-target']
+
+describe('product-editor', () => {
+  it('should show correct', async () => {
+    updateOrderLayout({ rows: 2, columns: 4, categories: [] })
+    // updateSelectedCategoryLayout({ name: 'Wine', color: '#FFF', rows: 4, columns: 6,  })
+    updateSelectedProductLayout({
+      type: 'Article',
+      product: mockProduct
+    })
+
+    // wipe-out components (to make dependencies component un-resolvable) to make sure snapshot render prop for stub class
+    ProductEditor2.components = {}
+
+    makeWrapper(ProductEditor2, {
+      shallow: false,
+      global: {
+        renderStubDefaultSlot: false,
+        stubs: {
+          portal: true,
+          'dialog-confirm-delete': true,
+          'color-selector': true,
+          'input-number': true,
+          'dialog-text-filter': true,
+          'g-select': true,
+          'g-switch': true,
+          'dialog-product-info': true,
+          'dialog-edit-popup-modifiers': true
+        },
+        config: {
+          isCustomElement: tag => _.includes(ignoreComponents, tag)
+        }
+      }
+    }, false)
+
+
+    await nextTick()
+    await delay(50)
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  // act
+})
