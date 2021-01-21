73d2b66fb6554ae82935f988f248304c8f335d94
anhoev <anhoev@yahoo.de>
2021-01-21 12:12:21 +0700

    refactore room/dashboard

--- components2/View/Dashboard/DashboardSidebar/PosDashboardSidebar2.vue
+++ components2/View/Dashboard/DashboardSidebar/PosDashboardSidebar2.vue
@@ -1,9 +1,74 @@
 <script>
-import DashboardSidebarUI from './DashboardSidebarUI'
 
-const { hooks, fn } = DashboardSidebarUI()
+import {GAvatar, GBtn, GImg, GSidebar, GSideBarTreeView, GSpacer} from "../../../../../../backoffice/pos-vue-framework";
+import {useI18n} from "vue-i18n";
+import DashboardSidebarItemsFactory from "./DashboardSidebarItems";
+import {onBeforeMount, onBeforeUnmount, ref} from "vue";
+import {avatar, user, username} from "../../../AppSharedStates";
+import {login} from "../../../Login/LoginLogic";
+import {getScopeAttrs} from "../../../../utils/helpers";
+import {genScopeId} from "../../../utils";
 
-export default fn()
+export default {
+  name: 'PosDashboardSidebar',
+  components: [GImg, GAvatar, GSidebar, GSpacer, GSideBarTreeView, GBtn],
+  props: {
+    items: Array
+  },
+  emit: ['toggle'],
+  setup(props, {slots, emit}) {
+    const {t: $t} = useI18n()
+    let timerInterval = ref(null)
+    const now = ref(dayjs().format('HH:mm'))
+    onBeforeMount(async function () {
+      //todo: remove auto login
+      if (!user.value) await login('0000')
+      timerInterval.value = setInterval(() => now.value = dayjs().format('HH:mm'), 1000)
+    })
+
+    onBeforeUnmount(() => {
+      if (timerInterval.value) timerInterval.value.clear()
+    })
+
+    const footerDefaultRenderFn = () => <g-btn uppercase={false} large text background-color="white"
+                                               text-color="#424242" width="100%" onClick={(e) => {
+      e.stopPropagation();
+      logout()
+    }}>
+      <g-icon svg>icon-logout</g-icon>
+      <span className="ml-2">{$t('sidebar.logOut')}</span>
+    </g-btn>
+
+    const sidebarSlots = {
+      header: () => <div class={['sidebar-header']} {...getScopeAttrs()}>
+        <g-avatar size="40">
+          <g-img src={avatar.value}></g-img>
+        </g-avatar>
+        <span class="username">{username.value}</span>
+        <g-spacer/>
+        <span>{now.value}</span>
+      </div>,
+      default: () => <>
+        {slots['above-tree-view'] ? slots['above-tree-view']() : null}
+        <g-side-bar-tree-view
+            {...getScopeAttrs()}
+            data={props.items}
+            onNodeSelected={node => node.onClick()}
+            onNodeExpansionToggled={() => emit('toggle')}
+        >
+        </g-side-bar-tree-view>
+        <slot name="above-spacer"/>
+        {slots['above-spacer'] && slots['above-spacer']()}
+        <g-spacer></g-spacer>
+        {slots['below-spacer'] && slots['below-spacer']()}
+        {slots['footer'] ? slots['footer']() : footerDefaultRenderFn()}
+      </>
+    }
+    return genScopeId(() => <g-sidebar medium style="height: 100vh; z-index: 2"
+                                       v-slots={sidebarSlots}>
+    </g-sidebar>)
+  }
+}
 </script>
 
 <style scoped lang="scss">
@@ -17,6 +82,7 @@ export default fn()
     line-height: 18px;
     font-weight: 600;
     color: #1d1d26;
+
     .username {
       word-break: break-all;
       -webkit-line-clamp: 2;
