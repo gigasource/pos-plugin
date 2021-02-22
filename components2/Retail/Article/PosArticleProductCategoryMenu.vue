f4a827dc09d26fa5f5772ef9886e307fe2e70ed7
ThinhVu <vutrongthinhk7@gmail.com>
2021-02-22 09:36:12 +0700

    [***] Bring back Pos-Article & fix route

---
+++ components2/Retail/Article/PosArticleProductCategoryMenu.vue
@@ -0,0 +1,76 @@
+<script>
+  import { onActivated, withModifiers } from 'vue'
+  import { genScopeId } from '../../utils';
+  import {
+    activeCategory, articleSelectedProductButton, articleSelectedColor,
+    getActiveProducts, updateArticleOrders
+  } from './pos-article-logic'
+  import {
+    categories
+  } from '../../Product/product-logic';
+  import {
+    loadCategories
+  } from '../../Product/product-logic-be';
+
+  export default {
+    name: 'PosArticleProductCategoryMenu',
+    setup() {
+      onActivated(async() => {
+        await loadCategories();
+        await select(categories.value[0])
+      })
+
+      async function select(item) {
+        activeCategory.value = item;
+        articleSelectedProductButton.value = null;
+        articleSelectedColor.value = null;
+        await getActiveProducts()
+        await updateArticleOrders();
+      }
+
+      return genScopeId(() => (
+          <div class="menu">
+            {
+              categories.value.map((item, i) =>
+                <g-btn class={[item === activeCategory.value ? 'menu__active' : '']}
+                       key={i} uppercase={false}
+                       onClick={withModifiers(() => select(item), ['stop'])}
+                       backgroundColor={'#fff'} elevation={0} height="100%"
+                       text-color={'#1d1d26'}>
+                  {item.name}
+                </g-btn>
+              )
+            }
+      </div>
+      ))
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+  .menu {
+    background-color: #90CAF9;
+    overflow-x: auto;
+    display: grid;
+    grid-template-rows: 1fr 1fr;
+    grid-auto-columns: 32%;
+    grid-gap: 6px;
+    grid-auto-flow: column;
+    padding: 6px;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+
+    &__active {
+      background-color: #2979FF !important;
+      color: white !important;
+    }
+
+    &:after {
+      content: '';
+      width: 1px;
+      height: 100%;
+    }
+  }
+</style>
--- manifest.js
+++ manifest.js
@@ -16,7 +16,8 @@ module.exports = {
     { loader: { type: 'route' }, filePath: 'components2/Inventory/InventoryStock.vue', name: 'InventoryStock', path: '/pos-inventory-stock' },
     { loader: { type: 'route' }, filePath: 'components2/Reservation/ReservationsList2.vue', name: 'Reservation', path: '/pos-reservation' },
     { loader: { type: 'route' }, filePath: 'components2/Retail/Order/PosOrderRetail.vue', name: 'Retail', path: '/retail--order' },
-    { loader: { type: 'route' }, filePath: 'components2/Retail/Order/PosOrderRetail', name: 'RetailRefund', path: '/retail--order-refund' },
+    { loader: { type: 'route' }, filePath: 'components2/Retail/Order/PosOrderRetail.vue', name: 'RetailRefund', path: '/retail--order-refund' },
+    { loader: { type: 'route' }, filePath: 'components2/Retail/Article/PosArticle.vue', name: 'Article', path: '/retail--article' },
     { loader: { type: 'route' }, filePath: 'components2/Customer/Customer.vue', name: 'Customer', path: '/pos-customer' },
     /// old components
     // { loader: { type: 'route' }, filePath: 'components2/Dashboard/DashboardSidebar/PosDashboardSidebar2.vue', name: 'Dashboard2', path: '/pos2-dashboard' },
