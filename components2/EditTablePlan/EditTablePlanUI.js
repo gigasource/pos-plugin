dcfbd5b5c3fc10c6405a2978b780593b0caf3038
ahcl7 <anhhungcolao77@gmail.com>
2021-03-01 16:28:29 +0700

    add dialogs, error message notifications for editTablePlan

--- components2/EditTablePlan/EditTablePlanUI.js
+++ components2/EditTablePlan/EditTablePlanUI.js
@@ -1,11 +1,21 @@
 import Hooks from 'schemahandler/hooks/hooks'
-import EditableRoom from '../TablePlan/EditableRoom/EditableRoom';
+import EditableRoom from './EditableRoom/EditableRoom';
 import EditTablePlanSidebar from './EditTablePlanSidebar';
-import { fetchRooms, onSelectRoom, roomsStates } from '../TablePlan/RoomState';
-import { getScopeAttrs } from '../../utils/helpers';
+import { fetchRooms, onSelectRoom, roomsStates, selectingRoomStates } from '../TablePlan/RoomState';
 import dialogTextFilter from '../pos-shared-components/dialogFilter/dialogTextFilter';
 import GSnackbar from '../../../../backoffice/pos-vue-framework/src/components/GSnackbar/GSnackbar';
 import { genScopeId } from '../utils';
+import {
+  dialog,
+  initI18n,
+  isSelectingARoom,
+  isSelectingARoomObject,
+  onUpdateSelectingRoomName,
+  selectingObject,
+  showExistedTableNameError,
+  tableNameExistedErrorMsg,
+  updateTableName
+} from './EditableRoom/EditTablePlanLogics';
 
 const EditablePlanFactory = () => {
   const hooks = new Hooks()
@@ -13,23 +23,24 @@ const EditablePlanFactory = () => {
     name: 'EditTablePlan',
     components: [EditTablePlanSidebar, EditableRoom, dialogTextFilter, GSnackbar],
     setup() {
+      initI18n()
       fetchRooms().then(() => {
         roomsStates.value.length > 0 && onSelectRoom(roomsStates.value[0])
       })
-      const sidebarRenderFn = genScopeId(() => <edit-table-plan-sidebar/>)
-      const roomRenderFn = () =>
-        <EditableRoom {...getScopeAttrs()}> </EditableRoom>
-
-      // const dialogsRenderFn = () => <>
-      // {isSelectingARoom.value  ? <dialog-text-filter label="Room name" default-value={selectingRoom.value.name} v-model={dialog.value.showRoomNameKbd} onSubmit={onUpdateSelectingRoomName}/> : null}
-      //   {isSelectingARoomObject.value  ? <dialog-text-filter label="Table name" default-value={selectingObject.value.name} v-model={dialog.value.showTableNameKbd} onSubmit={onUpdateTableName}/> : null}
-      // <g-snackbar v-model={showSnackBar.value} time-out="3000">{ tableNameExistedErrorMsg }</g-snackbar>
-      // </>
-
-      return () => <div class="row-flex">
+      const sidebarRenderFn = () => <EditTablePlanSidebar/>
+      const roomRenderFn = () => <EditableRoom/>
+      const dialogsRenderFn = () => <>
+        {isSelectingARoom.value &&
+        <dialog-text-filter label="Room name" default-value={selectingRoomStates.value.room.name} v-model={dialog.showRoomNameKbd} onSubmit={onUpdateSelectingRoomName}/>}
+        {isSelectingARoomObject.value &&
+        <dialog-text-filter label="Table name" default-value={selectingObject.value.name} v-model={dialog.showTableNameKbd} onSubmit={updateTableName}/>}
+        <g-snackbar v-model={showExistedTableNameError.value} time-out="3000">{tableNameExistedErrorMsg.value}</g-snackbar>
+      </>
+      return genScopeId(() => <div class="row-flex">
         {sidebarRenderFn()}
         {roomRenderFn()}
-      </div>
+        {dialogsRenderFn()}
+      </div>)
     }
   })
   return {
