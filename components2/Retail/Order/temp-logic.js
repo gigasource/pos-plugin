907fda366cb2ac41c9a752138b8c4bc3d3a3e955
ThinhVu <vutrongthinhk7@gmail.com>
2021-03-01 17:19:34 +0700

    Add logic to temp-logic.js

--- components2/Retail/Order/temp-logic.js
+++ components2/Retail/Order/temp-logic.js
@@ -1,11 +1,5 @@
 import { ref } from 'vue';
 
-export const OrderDisplayModes = {
-  Buy: 1,
-  Refund: 2
-}
-export const orderDisplayMode = ref(OrderDisplayModes.Buy)
-
 export const showDialogSavedList = ref(false)
 export const showDialogProductSearchResult = ref(false)
 export const showDialogChangePrice = ref(false)
@@ -17,10 +11,20 @@ export function /*PosStore::*/ updateNewPrice(val) {
 
 export function /*OrderStore::*/ chooseFunction() {
   console.error('OrderStore_chooseFunction')
+  // if (!functionName) return () => null
+  // return this[functionName]
 }
 
 export function /*OrderStore::*/ isActiveFnBtn(btn) {
   console.error('OrderStore_isActiveFnBtn', btn)
+  // if (!btn || !btn.buttonFunction) return
+  // if (btn.buttonFunction === 'changePrice' || btn.buttonFunction.includes('discount')) {
+  //   return !_.isNil(this.activeTableProduct) && !this.activeTableProduct.discountResistance
+  // }
+  // if (['pay', 'quickCash', 'saveOrder'].includes(btn.buttonFunction)) {
+  //   return this.currentOrder.items.length > 0
+  // }
+  // return true;
 }
 
 export const /*OrderStore::*/ updateTableRows = ref(() => {
@@ -43,10 +47,55 @@ export const /*OrderStore::*/ paymentChange = ref(60)
 export const /*OrderStore::*/ productIdQuery = ref('')
 export const /*OrderStore::*/ queryProductsById = () => {
   console.error('OrderStore::queryProductsById not impl')
+  // let quantity;
+  // if (this.productIdQuery.includes('x')) {
+  //   const queryStrArr = this.productIdQuery.split(' ')
+  //   quantity = parseInt(queryStrArr[2]);
+  //   this.productIdQuery = queryStrArr[0]
+  // }
+  // const results = cms.getList('Product').filter(item => item.id === this.productIdQuery)
+  // if (results) {
+  //   this.productIdQueryResults = results.map(product => ({
+  //     ...product,
+  //     originalPrice: product.price,
+  //     ...quantity && { quantity }
+  //   }))
+  // }
 }
 export const /*OrderStore::*/ productIdQueryResults = ref('')
 export const /*OrderStore::*/ getScrollWindowProducts = () => {
   console.error('OrderStore::getScrollWindowProducts not impl')
+  // const products = {}
+  // const allProducts = await cms.getModel('Product').find();
+  // const groupedProducts = _.groupBy(allProducts, 'category.name')
+  // const favouriteProducts = allProducts.filter(product => product.option && product.option.favorite)
+  // .sort((cur, next) => getProductGridOrder(cur, true) - getProductGridOrder(next, true))
+  // .map(product => ({
+  //   ..._.omit(product, 'attributes'),
+  //   originalPrice: product.price
+  // }))
+  // if (favouriteProducts) {
+  //   Object.assign(products, {
+  //     Favourite: _.chunk(favouriteProducts, 28)
+  //   })
+  // }
+  // if (groupedProducts) {
+  //   for (const key in groupedProducts) {
+  //     if (groupedProducts.hasOwnProperty(key)) {
+  //       const isFavourite = key === 'Favourite'
+  //       Object.assign(products, {
+  //         [key]: _.chunk(groupedProducts[key].sort((current, next) => {
+  //           return getProductGridOrder(current, isFavourite) - getProductGridOrder(next, isFavourite)
+  //         }).map(product => ({
+  //           ..._.omit(product, 'attributes'),
+  //           originalPrice: product.price
+  //         })), 28)
+  //       })
+  //     }
+  //   }
+  // }
+  // this.scrollWindowProducts = products
+  // return products
 }
 export const /*OrderStore::*/ currentOrder = ref(null)
 export const /*OrderStore::*/ activeTableProduct = ref(null)
@@ -56,16 +105,34 @@ export const /*OrderStore::*/ removeItemQuantity = () => {
 export const /*OrderStore::*/ savedOrders = ref([])
 export const /*OrderStore::*/ getSavedOrders = async () => {
   console.error('OrderStore::getSavedOrders not impl')
+  // try {
+  //   const orderModel = cms.getModel('Order')
+  //   this.savedOrders = await orderModel.find({ status: 'inProgress' })
+  // } catch (e) {
+  //   console.error(e)
+  // }
 }
 export const /*OrderStore::*/ resetOrderData = () => {
   console.error('OrderStore::resetOrderData not impl')
+  // await this.resetOrderData()
+  // const orderModel = cms.getModel('Order')
+  // this.currentOrder = await orderModel.findOne({ _id: order._id })
 }
 
 export const /*SettingsStore::*/ getAllCategories = async () => {
   console.error('SettingsStore::getAllCategories not impl')
+  // const categories = await cms.getModel('Category').find().sort('position')
+  // const posSettings = (await cms.getModel('PosSetting').find())[0];
+  // let favoriteArticle = posSettings.generalSetting.favoriteArticle;
+  // return favoriteArticle ? categories : categories.filter(cat => cat.name !== 'Favourite').map(c => ({
+  //   ...c,
+  //   position: c.position
+  // }))
 }
 export const /*SettingStore::*/ getProductLayout = () => {
   console.error('SettingsStore::getProductLayout not impl')
+  // const isFavourite = category && category.name === 'Favourite' || false
+  // return item.layouts && item.layouts.find(layout => !!layout.favourite === isFavourite) || {}
 }
 
 export const /*PosStore*/ activeCategory = ref(null)
