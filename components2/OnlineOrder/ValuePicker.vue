964184ce79a70e68b4752dcf7573a4e1bd56a54f
ThinhVu <vutrongthinhk7@gmail.com>
2021-03-01 14:24:31 +0700

    Correct ValuePicker.vue

--- components2/OnlineOrder/ValuePicker.vue
+++ components2/OnlineOrder/ValuePicker.vue
@@ -1,8 +1,9 @@
 <script>
-import { withModifiers, ref, onBeforeMount } from 'vue'
+import { withModifiers, ref, onMounted, nextTick } from 'vue'
 import { useI18n } from 'vue-i18n';
 import { GBtnBs, GIcon, GTextFieldBs } from '../../../../backoffice/pos-vue-framework';
 import dialogNumberFilter from '../pos-shared-components/dialogFilter/dialogNumberFilter';
+import { genScopeId } from '../utils';
 export default {
   props: {
     values: Array,
@@ -11,17 +12,14 @@ export default {
     defaultValue: null
   },
   components: [GBtnBs, GTextFieldBs, dialogNumberFilter, GIcon],
-  setup(props, { emit }) {
+  setup(props, { emit, slots }) {
     const { t } = useI18n()
-    const values = ref(props.values || [])
-    const modelValue = ref(props.modelValue)
-    const defaultValue = ref(props.defaultValue)
-    const allowCustom = ref(props.allowCustom)
-    const inputValue = ref((!values.value.includes(modelValue.value) && modelValue.value) || '')
+    const inputValue = ref((!props.values.includes(props.modelValue) && props.modelValue) || '')
     const dialog = ref(false)
     const inputRef = ref(null)
+
     function pickValue(val) {
-      if (allowCustom.value) {
+      if (props.allowCustom) {
         inputValue.value = null
       }
       emit('update:modelValue', val)
@@ -30,38 +28,39 @@ export default {
       emit('update:modelValue', +val)
     }
     function isValueSelected(val) {
-      return modelValue === val
+      return props.modelValue === val
     }
     function changeValue(val) {
       inputValue.value = +val
       pickCustomValue(val)
     }
-    onBeforeMount(() => {
-      if (!modelValue.value && defaultValue.value) emit('update:modelValue', defaultValue.value)
 
+    onMounted(() => {
+      nextTick(() => {
+        if (!props.modelValue && props.defaultValue) {
+          emit('update:modelValue', props.defaultValue)
+        }
+      })
     })
 
-    return () =>
+    return genScopeId(() =>
         <div class="picker-wrapper">
-          {values.value.map(item =>
-              <slot name="item" item={item} select={pickValue} isSelected={isValueSelected}>
-                <g-btn-bs onClick={withModifiers(() => pickValue(item), ['stop'])} class={['mb-1', isValueSelected(item) && 'selected']} border-color="#C4C4C4" text-color="black" width="40" height="30">
-                  {item}
-                </g-btn-bs>
-              </slot>
-          )}
-          {
-            (allowCustom) &&
-            <g-text-field-bs ref={inputRef} placeholder={t('onlineOrder.customTime')} modelValue={inputValue.value} onUpdate:modelValue={pickCustomValue} v-slots={{
-              'append-inner': () =>
-                  <g-icon style="cursor: pointer" onClick={() => dialog.value = true}>
-                    icon-keyboard
-                  </g-icon>
-            }}>
-            </g-text-field-bs>
+          {props.values.map(item => (slots.item && slots.item({ item, select: pickValue, isSelected: isValueSelected })) ||
+              <g-btn-bs onClick={withModifiers(() => pickValue(item), ['stop'])}
+                        class={['mb-1', isValueSelected(item) && 'selected']}
+                        border-color="#C4C4C4" text-color="black"
+                        width="40" height="30">
+                {item}
+              </g-btn-bs> )}
+          { props.allowCustom &&
+            <g-text-field-bs ref={inputRef} placeholder={t('onlineOrder.customTime')}
+                             modelValue={inputValue.value} onUpdate:modelValue={pickCustomValue} v-slots={{
+              'append-inner': () => <g-icon style="cursor: pointer" onClick={() => dialog.value = true}>icon-keyboard</g-icon>
+            }}/>
           }
-          <dialog-number-filter v-model={dialog.value} label="Custom time" onSubmit={changeValue}></dialog-number-filter>
+          <dialog-number-filter v-model={dialog.value} label="Custom time" onSubmit={changeValue}/>
         </div>
+    )
   }
 }
 </script>
