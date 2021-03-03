27e21e8fe5ceebaead9ec40cf3860a2aba84383e
ThinhVu <vutrongthinhk7@gmail.com>
2021-03-03 14:46:04 +0700

    [Pos Order Retail] Re-struct DOM tree

--- components2/Retail/Order/PosRetailCart.vue
+++ components2/Retail/Order/PosRetailCart.vue
@@ -19,98 +19,33 @@ export default {
     isRefundMode: Boolean
   },
   setup(props) {
+    const router = useRouter()
     const { isRefundMode } = props
     const { t } = useI18n()
     const order = getCurrentOrder()
-
     const total = computed(() => {
       return order.items.reduce((totalPrice, item) => {
         totalPrice += item.price * item.quantity
         return totalPrice
       }, 0)
     })
-    const router = useRouter()
-
-    function back() {
-      router.go(-1)
-    }
-
-    function toPayment() {
-      router.push({ path: '/pos-payment' })
-    }
-
-    function generateRandomColor() {
-      return '#' + Math.floor(Math.random() * 16777215).toString(16);
-    }
-
-    const showCtxMenu = ref(false)
-    const showScreenSettingPanel = ref(false)
-
-    function renderScreenSettingPanel() {
-      function renderNumericPropSetting(title, prop) {
-        const valueStyle = { width: '50px', fontSize: '12px', fontWeight: 'bold' }
-        const propStyle = { fontSize: '12px' }
-        return (
-            <div class="row-flex align-items-center">
-              <span style={propStyle}>{title}</span>
-              <g-spacer/>
-              <g-icon onClick={() => changeValue(prop, retailLayoutSetting[prop] - 1)}>remove_circle</g-icon>
-              <span style={valueStyle}>{retailLayoutSetting[prop]}</span>
-              <g-icon onClick={() => changeValue(prop, retailLayoutSetting[prop] + 1)}>add_circle</g-icon>
-            </div>
-        )
-      }
-
-      function renderBooleanPropSetting(title, prop) {
-        const propStyle = { fontSize: '12px' }
-        return (
-            <div class="row-flex align-items-center">
-              <span style={propStyle}>{title}</span>
-              <g-spacer/>
-              <g-switch v-model={retailLayoutSetting[prop]}></g-switch>
-            </div>
-        )
-      }
-
-      const sectionStyle = { marginTop: '10px', position: 'relative', border: '1px solid #444', paddingBottom: '10px' }
-      const sectionTitleStyle = { paddingLeft: '5px', backgroundColor: '#ddd', marginBottom: '8px' }
-      const propSectionStyle = { marginLeft: '5px' }
-
-      return <>
-        <div style={sectionStyle}>
-          <div style={sectionTitleStyle}>Product</div>
-          <div style={propSectionStyle}>
-            {renderNumericPropSetting('Rows', 'productRow')}
-            {renderNumericPropSetting('Columns', 'productColumn')}
-            {renderNumericPropSetting('Font size', 'productFontSize')}
-            {renderBooleanPropSetting('Show full name', 'showFullProductName')}
-          </div>
-        </div>
-
-        <div class="row-flex justify-end">
-          <g-btn-bs width="100" small style="margin-top: 10px; margin-right:0" background-color="#1271FF"
-                    onClick={() => {
-                      saveRetailLayoutSetting()
-                      showScreenSettingPanel.value = false
-                    }}>Save
-          </g-btn-bs>
-        </div>
-      </>
-    }
+    const footerHeight = '36px'
+    const cartHeightStyle = `max-height: calc(100% - ${footerHeight}); height: calc(100% - ${footerHeight})`
 
     function decreaseQty(item) {
       changeItemQuantity(order, item, -1)
     }
-
     function increaseQty(item) {
       if (isRefundMode && item.quantity === item.maxQuantity)
         return
       changeItemQuantity(order, item, 1)
     }
-
+    function generateRandomColor() {
+      return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    }
     function renderCartItems() {
       return (
-          <div class="detail-table">
+          <div class="detail-table" style={cartHeightStyle}>
             {order.items.map((item, i) =>
                 <div key={i} class="detail-table__row">
                   <div class="detail-table__row-main">
@@ -138,60 +73,13 @@ export default {
     const payable = computed(() => {
       return order.items && order.items.length && _.some(order.items, i => i.quantity > 0)
     })
-
-    const customer = ref(null)
-    const showCustomerDialog = ref(false)
-    function openSelectCustomerDialog() {
-      showCustomerDialog.value = true
-    }
-    function renderCustomer() {
-      return <div style="cursor: pointer" onClick={openSelectCustomerDialog}>
-        { !customer.value
-            ? <div>
-                Select customer
-                <g-dialog v-model={showCustomerDialog.value}></g-dialog>
-              </div>
-            : <div>
-                <p style="color: #2979FF">{customer.value.name}</p>
-                <p style="color: #757575">{customer.value.phone}</p>
-              </div>
-        }
-      </div>
-    }
-    function renderHeader() {
-      return (
-          <div class="detail-header">
-            { renderCustomer() }
-            <g-spacer/>
-            <g-menu v-model={showCtxMenu.value} close-on-content-click v-slots={{
-              default: () => (
-                  <g-expand-x-transition>{execGenScopeId(() =>
-                      <div style="background-color: #FFF;" class="col-flex">
-                        <g-btn-bs icon="icon-blue-cog" onClick={() => {
-                          loadRetailLayoutSetting()
-                          showScreenSettingPanel.value = true
-                        }}>Edit Screen
-                        </g-btn-bs>
-                        <g-btn-bs icon="icon-back" onClick={back}>
-                          Back
-                        </g-btn-bs>
-                      </div>
-                  )}</g-expand-x-transition>
-              ),
-              activator: ({ on }) => (
-                  <g-btn rounded outlined flat onClick={on.click} style="border-radius: 50%; width: 30px; height: 30px; text-align: center; min-width: 30px; padding: 0">
-                    <b>&#183; &#183; &#183;</b>
-                  </g-btn>
-              )
-            }}/>
-          </div>
-      )
-    }
-
     const showFooterCtxMenu = ref(false)
+    function toPayment() {
+      router.push({ path: '/pos-payment' })
+    }
     function renderFooter() {
       return (
-          <div class="row-flex mt-1" style="cursor: pointer">
+          <div class="row-flex mt-1" style={`cursor: pointer; height: ${footerHeight}; max-height: ${footerHeight}`}>
             <g-menu v-model={showFooterCtxMenu.value} close-on-content-click top v-slots={{
               default: () => (
                   <g-expand-x-transition>{execGenScopeId(() =>
@@ -218,11 +106,9 @@ export default {
           </div>
       )
     }
-
     return genScopeId(() => (
         <div class="detail">
-          {renderHeader()}
-          {showScreenSettingPanel.value ? renderScreenSettingPanel() : renderCartItems()}
+          {renderCartItems()}
           {renderFooter()}
         </div>
     ))
@@ -235,25 +121,6 @@ export default {
   background-color: white;
   display: flex;
   flex-direction: column;
-  height: 100%;
-  overflow: hidden;
-
-  &-header {
-    display: flex;
-    align-items: center;
-    margin-bottom: 8px;
-
-    .username {
-      word-break: break-all;
-      -webkit-line-clamp: 2;
-      display: -webkit-box;
-      -webkit-box-orient: vertical;
-      overflow: hidden;
-      font-size: 14px;
-      font-weight: 600;
-      padding-left: 8px;
-    }
-  }
 
   &-table {
     flex: 1;
