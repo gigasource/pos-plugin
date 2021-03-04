bffb8cbe6e3a5caf34e7425563a2e83d8b5cb062
ThinhVu <vutrongthinhk7@gmail.com>
2021-03-04 16:29:37 +0700

    Automatically navigate to pos-login if the user is not logged in

--- components2/EmptyStore.vue
+++ components2/EmptyStore.vue
@@ -1,15 +1,34 @@
 <script>
 import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import { user } from './AppSharedStates';
 import cms from 'cms'
+
 // This file is for emitting screen-loaded
 export default {
   name: 'EmptyStore',
   domain: 'EmptyStore',
   props: {},
   setup() {
+    // TODO: navigate to login page if the user is not logged-in
+    // Issue: not work as the Store loaded same time with component, only work in next route navigation.
+    // const router = useRouter()
+    // router.beforeEach((to, from, next) => {
+    //   console.log('beforeEach', to, from, next, user.value)
+    //   if (to.path === '/admin' || to.path === '/plugins' || to.path === '/pos-login' || to.path === '/pos-setup') {
+    //     next()
+    //   } else if (!user.value) {
+    //     next('/pos-login')
+    //   } else {
+    //     next()
+    //   }
+    // })
+
     onMounted(() => {
       cms.socket.emit('screen-loaded')
     })
   }
+
+
 }
 </script>
