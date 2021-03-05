47021a4c3f08653dae458534145edd9a5414953d
ahcl7 <anhhungcolao77@gmail.com>
2021-03-05 09:54:50 +0700

    convert some dialogs to jsx

--- components2/pos-shared-components/dialogFilter/dialogRangeFilter.vue
+++ components2/pos-shared-components/dialogFilter/dialogRangeFilter.vue
@@ -1,108 +1,104 @@
-<template>
-	<g-dialog v-model="internalValue" width="70%" eager :fullscreen="isMobile">
-		<div class="wrapper">
-			<div class="header">
-				<span>{{label}}</span>
-				<g-icon @click="internalValue = false" size="20" svg>icon-close</g-icon>
-			</div>
-			<div>
-				<pos-range-slider :min="min" :max="max" v-model="filter"/>
-				<div class="range-value">
-					<span>{{min === 0 ? min : '€ ' + min}}</span>
-					<span>{{$t('common.currency', storeLocale)}} {{max}}</span>
-				</div>
-			</div>
-			<g-spacer/>
-			<div class="buttons">
-				<g-btn :uppercase="false" text @click="internalValue = false" outlined width="120" style="margin-right: 8px">{{$t('ui.cancel')}}</g-btn>
-				<g-btn :uppercase="false" text @click="submit" backgroundColor="#2979FF" text-color="#FFFFFF" width="120">{{$t('ui.ok')}}</g-btn>
-			</div>
-		</div>
-	</g-dialog>
-</template>
-
 <script>
-	import PosRangeSlider from '../POSInput/PosRangeSlider';
-	
-  export default {
-    name: 'dialogRangeFilter',
-		injectService: ['PosStore:(storeLocale, isMobile)'],
-		components: {PosRangeSlider},
-    props: {
-      label: null,
-      min: {
-        type: [Number, String],
-        default: 0
-      },
-      max: {
-        type: [Number, String],
-        default: 500
-      },
-      modelValue: null,
+import { execGenScopeId, genScopeId, internalValueFactory } from '../../utils';
+import { useI18n } from 'vue-i18n';
+import { isMobile } from '../../AppSharedStates';
+import { ref, watch } from 'vue'
+
+export default {
+  props: {
+    label: null,
+    min: {
+      type: [Number, String],
+      default: 0
     },
-    data() {
-      return {
-        filter: [0, 0]
-      }
+    max: {
+      type: [Number, String],
+      default: 500
     },
-    computed: {
-      internalValue: {
-        get() {
-          return this.modelValue || false
-        },
-        set(value) {
-          this.$emit('update:modelValue', value)
-        }
-      }
-    },
-    methods: {
-      async submit() {
-        this.$emit('submit', this.filter);
-        this.internalValue = false;
-      }
-    },
-		watch: {
-			internalValue: function() {
-				this.filter = [0, 0]
-			}
-		}
+    modelValue: null,
+  },
+  emits: ['update:modelValue', 'submit'],
+  setup(props, { emit }) {
+    const { t, locale } = useI18n()
+    const internalValue = internalValueFactory(props, { emit })
+    const filter = ref([0, 0])
+
+    function submit() {
+      emit('submit', filter);
+      internalValue.value = false;
+    }
+
+    watch(() => internalValue.value, () => {
+      filter.value = [0, 0]
+    })
+    return genScopeId(() => <>
+          <g-dialog v-model={internalValue.value} width="70%" eager fullscreen={isMobile.value}>
+            {execGenScopeId(() => <div class="wrapper">
+              <div class="header">
+                <span>
+                  {props.label} </span>
+                <g-icon onClick={() => internalValue.value = false} size="20" svg>
+                  icon-close
+                </g-icon>
+              </div>
+              <div>
+                <pos-range-slider min={props.min} max={props.max} v-model={filter.value}></pos-range-slider>
+                <div class="range-value">
+                  <span>
+                    {props.min === 0 ? props.min : '€ ' + props.min} </span>
+                  <span>
+                    {t('common.currency', locale.value)} {props.max} </span>
+                </div>
+              </div>
+              <g-spacer></g-spacer>
+              <div class="buttons">
+                <g-btn uppercase={false} text onClick={() => internalValue.value = false} outlined width="120" style="margin-right: 8px">
+                  {t('ui.cancel')} </g-btn>
+                <g-btn uppercase={false} text onClick={submit} backgroundcolor="#2979FF" text-color="#FFFFFF" width="120">
+                  {t('ui.ok')} </g-btn>
+              </div>
+            </div>)}
+          </g-dialog>
+        </>
+    )
   }
+}
 </script>
 
 <style scoped lang="scss">
-	.wrapper {
-		width: 100%;
-		background-color: #FFFFFF;
-		display: flex;
-		flex-direction: column;
-	}
+.wrapper {
+  width: 100%;
+  background-color: #FFFFFF;
+  display: flex;
+  flex-direction: column;
+}
 
-	.header {
-		display: flex;
-		align-items: flex-start;
-		justify-content: space-between;
-		padding: 16px 16px 48px 30px;
-		font-weight: bold;
-		font-size: 16px;
-		line-height: 20px;
-	}
+.header {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  padding: 16px 16px 48px 30px;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 20px;
+}
 
-	.range-value {
-		display: flex;
-		align-items: flex-start;
-		justify-content: space-between;
-		padding: 0 30px;
-	}
+.range-value {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  padding: 0 30px;
+}
 
-	.buttons {
-		display: flex;
-		align-items: center;
-		justify-content: flex-end;
-		padding: 16px;
+.buttons {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 16px;
 
-		.g-btn__outlined {
-			border: 1px solid #979797;
-			color: #1d1d26;
-		}
-	}
+  .g-btn__outlined {
+    border: 1px solid #979797;
+    color: #1d1d26;
+  }
+}
 </style>
