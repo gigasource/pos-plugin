4fd9e71a3634c1ac7e68b2e9488a3665e2fc57bd
ThinhVu <vutrongthinhk7@gmail.com>
2021-03-08 15:59:31 +0700

    Switch app type

--- components2/AppType.js
+++ components2/AppType.js
@@ -4,4 +4,4 @@ export const appType = {
 	POS_RESTAURANT: 'POS_RESTAURANT',
 	POS_RETAIL: 'POS_RETAIL'
 }
-export const currentAppType = ref(appType.POS_RETAIL)
+export const currentAppType = ref(appType.POS_RESTAURANT)
