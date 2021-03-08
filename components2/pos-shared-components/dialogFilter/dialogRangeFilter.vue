c758b68e04ec95b4afd568257b6b60bbb1a4cbf0
ThinhVu <vutrongthinhk7@gmail.com>
2021-03-08 18:43:37 +0700

    [dialogRangeFilter] Fix bug emit ref object instead of value

--- components2/pos-shared-components/dialogFilter/dialogRangeFilter.vue
+++ components2/pos-shared-components/dialogFilter/dialogRangeFilter.vue
@@ -24,7 +24,7 @@ export default {
     const filter = ref([0, 0])
 
     function submit() {
-      emit('submit', filter);
+      emit('submit', filter.value);
       internalValue.value = false;
     }
 
